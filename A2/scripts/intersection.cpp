5b5c9280ffadd77b10268b732d6b611232b7f1bf
David Levin <dilevin@csail.mit.edu>
2018-09-25 16:02:47 -0400

    Add due date to A2 handout

--- A2/scripts/intersection.cpp
+++
@@ -1,7 +0,0 @@
-auto s1 = sphere(2.0, {-0.5, 0.5, 0.5});
-auto s2 = sphere(2.0, {0.5, 0.5, 0.5});
-auto s1ns2 = intersectAB(s1,s2);
-
-Region<3> r({ -5, -5, -5 }, { 5, 5, 5 });
-
-auto mesh = Mesh::render(s1ns2, r, 0.025);
